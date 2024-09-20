4efc274a2f6b0d8c73934723c3ec09178f38ab56
rafael-sapalo <rafael.sapalo@outlook.com>
2024-09-20 10:19:57 +0200

    fix(frontend): Fix import formatting in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { NextUIProvider } from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
 import App from './App.tsx'
 import './index.css'
 
