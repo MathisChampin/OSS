97329d79cfa6a07685bfa2248f5afb6ef6af42ae
rafael-sapalo <rafael.sapalo@outlook.com>
2024-10-03 12:34:05 +0200

    feat(frontend): add navbar components and image processing

--- frontend/deuxieme-ver/src/main.tsx
+++ frontend/deuxieme-ver/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 import App from './App.tsx'
 import './index.css'
+import Navbar from './components/Navbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
