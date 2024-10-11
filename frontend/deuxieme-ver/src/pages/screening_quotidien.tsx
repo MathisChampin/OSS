407cd5653f7a2a7ce14f83d79099cefcb805c85c
rafael-sapalo <rafael.sapalo@outlook.com>
2024-10-11 09:49:20 +0200

    feat: add base of new page

--- frontend/deuxieme-ver/src/pages/screening_quotidien.tsx
+++ frontend/deuxieme-ver/src/pages/screening_quotidien.tsx
@@ -1,16 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function ScreeningQuotidien() {
+    const [username,    setUsername] = useState<string>("");
+    const [hospitalName, setHospitalName] = useState<string>("");
+
     useEffect(() => {
+        setUsername("hello");
+        setHospitalName("ede")
         console.log("Screening Quotidien");
     }, []);
     return (
         <div>
            <div>
-                <h1>Screening Quotidien</h1>
+                <h1>Screening quotidien des malades présents et non admis</h1>
            </div>
            <div>
                 <h1>Nombre de lits "réanimation" ouverts</h1>
+                <p>Nom: {username}</p>
+                <p>Nom de l'hospital: {hospitalName}</p>
            </div>
         </div>
     )
